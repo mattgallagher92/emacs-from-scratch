1b12b5fc8d652274b7f9b3774a19686db8fd77df
Matt Gallagher <mjg1992@hotmail.co.uk>
2023-06-21 08:57:03 +0100

    Remove custom bindings for tab in lsp/company mode

--- init.el
+++ init.el
@@ -493,10 +493,6 @@
 (use-package company
   :after lsp-mode
   :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-         ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
