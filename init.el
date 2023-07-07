47a3508614deb5dd47b998177c1c4fca6e42068f
Matt Gallagher <mjg1992@hotmail.co.uk>
2023-07-07 21:50:14 +0100

    Add dired configuration

--- init.el
+++ init.el
@@ -70,3 +70,21 @@
   :init (setenv "DOTNET_ROOT" "/usr/share/dotnet")
   :defer t
   :hook (fsharp-mode . lsp-deferred))
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h" 'dired-single-up-directory
+    "l" 'dired-single-buffer))
+
+(use-package dired-single
+  :commands (dired dired-jump))
+
+(use-package all-the-icons)
+
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
